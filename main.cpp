579eef873b0c557a74a4ec8fd7cdfb366fb4e4f4
AndreiYSF <iosif.andrei.stefan@gmail.com>
2025-04-01 22:25:44 +0300

    added Radix, Merge, Shell

--- main.cpp
+++ main.cpp
@@ -1,9 +1,232 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//void fc() {
+//
+//    auto start = chrono::high_resolution_clock::now();
+//
+//    this_thread::sleep_for(std::chrono::milliseconds(500));
+//
+//    auto end = chrono::high_resolution_clock::now();
+//
+//    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+//
+//    cout << duration.count();
+//}
+
+
+void display(vector <double> v) {
+
+    for (auto &i : v)
+        cout << i << ' ';
+    cout << '\n';
+}
+
+// verifica daca un vector este sortat
+bool isSorted(vector<double> &v) {
+
+    for (int i = 0; i < v.size() - 1; i++)
+        if (v[i] > v[i + 1])
+            return false;
+    return true;
+}
+
+// RadixSort clasic
+void RadixSort1(vector<double> &v) {
+
+    vector<int> aux;
+
+    for (auto &i: v) {
+
+        aux.push_back(i);
+        if (floor(i) != i) {
+
+            aux.clear();
+            cout << "RadixSort1 nu a putut executa sortarea.\n";
+            return;
+        }
+    }
+
+    for (int k = 0; k <= 30; k++) {
+
+        bool ok = true; // verifica daca totate elementele sunt mai mici strict decat 2^k
+
+        int pow = (1 << k);
+
+        queue<int> leftSide, rightSide;
+
+        for (int i = 0; i < aux.size(); i++) {
+
+            if (aux[i] >= pow)
+                ok = false;
+
+            if (aux[i] & pow)
+                rightSide.push(aux[i]);
+            else
+                leftSide.push(aux[i]);
+        }
+
+        aux.clear();
+        while (!leftSide.empty())
+            aux.push_back(leftSide.front()), leftSide.pop();
+        while (!rightSide.empty())
+            aux.push_back(rightSide.front()), rightSide.pop();
+
+        if (ok)
+            break;
+    }
+
+    v.clear();
+    for (auto &i: aux)
+        v.push_back(i);
+}
+
+// RadixSort baza 10
+void RadixSort2(vector<double> &v) {
+
+    vector<int> aux;
+
+    for (auto &i: v) {
+
+        aux.push_back(i);
+        if (floor(i) != i) {
+
+            aux.clear();
+            cout << "RadixSort1 nu a putut executa sortarea.\n";
+            return;
+        }
+    }
+
+    queue<int> Q[10];
+
+    for (int k = 0, pow = 1; k <= 9; k++, pow *= 10) {
+
+        bool ok = true; // verifica daca totate elementele sunt mai mici strict decat 10^k
+
+        for (int i: aux) {
+
+            if (i >= pow)
+                ok = false;
+
+            int cif = (i / pow) % 10;
+
+            Q[cif].push(i);
+        }
+
+        aux.clear();
+        for (auto &i: Q)
+            while (!i.empty())
+                aux.push_back(i.front()), i.pop();
+
+        if (ok)
+            break;
+    }
+
+    v.clear();
+    for (auto &i: aux)
+        v.push_back(i);
+}
+
+// RadixSort baza 2^16 = 65636
+void RadixSort3(vector<double> &v) {
+
+    const int b = 65636;
+
+    vector<int> aux;
+
+    for (auto &i: v) {
+
+        aux.push_back(i);
+        if (floor(i) != i) {
+
+            aux.clear();
+            cout << "RadixSort1 nu a putut executa sortarea.\n";
+            return;
+        }
+    }
+
+    queue<int> Q[b];
+
+    for (int pow: {1, b}) {
+
+        for (int i: aux) {
+
+            int cif = (i / pow) % b;
+
+            Q[cif].push(i);
+        }
+
+        aux.clear();
+        for (auto &i: Q)
+            while (!i.empty())
+                aux.push_back(i.front()), i.pop();
+    }
+
+    v.clear();
+    for (auto &i: aux)
+        v.push_back(i);
+}
+
+void MergeSort(vector<double> &v) {
+
+    if (v.size() == 1)
+        return;
+
+    vector<double> left, right;
+
+    int i = 0;
+
+    while (left.size() < (v.size() / 2))
+        left.push_back(v[i++]);
+    while (i < v.size())
+        right.push_back(v[i++]);
+
+    MergeSort(left);
+    MergeSort(right);
+
+    i = 0;
+    int j = 0, k = 0;
+
+    while (i < left.size() && j < right.size())
+        if (left[i] < right[j])
+            v[k++] = left[i++];
+        else
+            v[k++] = right[j++];
+
+    while (i < left.size())
+        v[k++] = left[i++];
+    while (j < right.size())
+        v[k++] = right[j++];
+}
+
+void ShellSort(vector<double> &v) {
+
+    int n = v.size();
+
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+
+        for (int i = gap; i < n; i++) {
+
+            double aux = v[i];
+            int j = i;
+
+            while (j >= gap && v[j - gap] > aux) {
+
+                v[j] = v[j - gap];
+                j -= gap;
+            }
+            v[j] = aux;
+        }
+    }
+}
+
 int main() {
 
+    vector<double> v = {1, 8, 2, 55, 112, 12, 342, 2};
 
+    ShellSort(v);
+
+    cout << isSorted(v);
 
     return 0;
 }
